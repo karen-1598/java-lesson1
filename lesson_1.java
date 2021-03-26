e405aebaab8b2c876b8b002a9a970313e876c7a0
Karen <81428601+karen-1598@users.noreply.github.com>
2021-03-26 22:03:18 +0400

    Add files via upload

---
+++ lesson_1.java
@@ -0,0 +1,35 @@
+public class lesson_1 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+
+        byte a=100;
+        System.out.println("byte a = "+a);
+
+        short b=2135;
+        System.out.println("short b = "+b);
+
+        int c=45615616;
+        System.out.println("int c = "+c);
+
+        long d=458464315656L+c;
+        System.out.println("long d = "+d);
+
+        float e=14856.48f;
+        System.out.println("float e = "+e);
+
+        double f=b+a+45.8;
+        System.out.println("double f = "+f);
+
+        boolean j=false;
+        System.out.println("boolean j = "+j);
+
+        char h='^';
+        System.out.println("char h = "+h);
+
+        String i=h+"2";
+        System.out.println("String i = "+i);
+
+
+    }
+}
